c112f1e3fdf40e02548d70a44e4377331a0cd431
Iaroslav Baslin <wertout77@gmail.com>
2022-11-16 21:49:05 +0300

    Experimental image filtering

---
+++ PSF.py
@@ -0,0 +1,44 @@
+"""Image convolution using kernel of a matrix which is generated on the fly."""
+
+from typing import List
+
+import numpy
+from PIL import Image
+from PIL import ImageFilter as IFil
+
+
+def generate_conv_matrix(size: int) -> List[List]:
+    pass
+
+
+def matrix_convolution(img_data_vec: List) -> List:
+    box_blur = [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ]
+    pass
+
+
+def gaussian_convolution(image_as_vec: List) -> List:
+    pass
+
+
+def main() -> None:
+    image = Image.open("samples/pic1.png")
+    print(image.getbands())
+    image_colors = list(image.getdata())    # List of (R, G, B) pixel by pixel
+
+    for row in image_colors:
+        print(row)
+
+    i_filtered = image.filter(IFil.BoxBlur(radius=0.5))
+    i_filtered.show(title="AfterBoxBlur")
+    i_filtered.filter(IFil.GaussianBlur(radius=1)).show(title="AfterGauBlur")
+    print("La Finalle")
+    for row in i_filtered.getdata():
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# image-restoration
-Stochastic Matrix-Iteration Method
+# Image Restoration
+Using Stochastic Matrix-Iteration Method. 
+
+Original paper: [we_need_link_here]
+
+# Quick Instruction
+
+1. Python's Virtual environment
+2. ```pip install -r requirements.txt```
+3. **TBD**
+
+
