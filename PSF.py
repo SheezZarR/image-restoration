9abf91c6307a0c3268ae5f683572c1c771ec42ec
Bastard12 <103337840+Bastard12@users.noreply.github.com>
2022-11-20 12:38:22 +0300

    Update PSF.py

--- PSF.py
+++ PSF.py
@@ -25,21 +25,6 @@ def gaussian_convolution(image_as_vec: List) -> List:
     pass
 
 
-'''Функция которая получает на вход g и L, а на выход возвращает массив g_доп длинной L'''
-def g_additional(g, L):
-    g_dop = np.zeros(L)
-
-    for i in range(L):
-        n_dop = np.random.normal(0, 0.1, g[0].size)
-        g.dop[i] = g + n_dop
-
-    return np.matrix(g_dop)
-
-
-'''Функция которая получает на вход f_вост и L, на выход возвращает f_ст'''
-def f_stochastic(f, L):
-
-    return sum(f)/L
 
 
 def main() -> None:
