1e041d554952e748f49adf716d8001b97e39a296
Bastard12 <rus.akopyan03@mail.ru>
2022-12-01 16:05:04 +0300

    Rusik

--- g_additional.py
+++ g_additional.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-'''Функция которая получает на вход g и L, а на выход возвращает массив g_доп длинной L'''
-def g_additional(g, L):
-    g_dop = np.zeros(L)
+'''Функция которая получает на вход g, sigma и L, а на выход возвращает массив g_доп длинной L'''
+def g_additional(g, sigma, L):
+
+    g_dop = [0]*L
 
     for i in range(L):
-        n_dop = np.random.normal(0, 0.1, g[0].size)
-        g.dop[i] = g + n_dop
+        n_dop = np.random.normal(0, sigma, g[0].size)
+        print(n_dop, g)
+        g_dop[i] = g + n_dop
 
     return g_dop
 
@@ -14,4 +16,7 @@ def g_additional(g, L):
 '''Функция которая получает на вход f_вост и L, на выход возвращает f_ст'''
 def f_stochastic(f, L):
 
-    return sum(f)/L
+    return np.sum(f)/L
+
+
+
